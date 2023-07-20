6006973498331f754882665267c30a422c7b0b79
Ammar <100584308+AMMARALBESANI@users.noreply.github.com>
2023-07-20 06:17:56 +0300

    all done

---
+++ codechallenge12/codechallenge12/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace codechallenge12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           myQueue myqueue = new myQueue();
+            myqueue.enqueue(new Animals { Species = "cat", Name = "gg" }); 
+            myqueue.enqueue(new Animals { Species = "cat", Name = "tog" }); 
+            myqueue.enqueue(new Animals { Species="dog", Name="ghg"});
+              var x = myqueue.dequeue("dog");
+           Console.WriteLine($"name is: {x.Name} species is: {x. Species}" );
+           // Console.WriteLine(myqueue.print());
+
+        }
+    }
+}
